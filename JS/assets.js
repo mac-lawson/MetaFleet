5096491d5ed104036900a1bcff70f60f1c8468f4
Mac Lawson <macattackairman@gmail.com>
2022-06-30 15:47:06 -0400

    work at WPI done

---
+++ JS/assets.js
@@ -0,0 +1,10 @@
+function createNewAsset() {
+    var assetIP = document.getElementById("assetIP").value;
+
+    var assetName = document.getElementById("assetName").value;
+
+    document.cookie((assetIP) + '=' + (assetName))
+    document.cookie("headset=" + (assetName))
+
+    document.getElementById("ACTIVE_DEPLOYS").innerText = (document.cookie);
+}
--- index.php
+++ index.php
@@ -83,49 +83,32 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <h5 class="modal-title" id="staticBackdropLabel">Deploy New Assets to MetaFleet</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
               <div class="container-sm" >
-            <div class="input-group mb-3">
-            <button type="button" class="btn btn-outline-secondary">Action</button>
-            <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-              <span class="visually-hidden">Toggle Dropdown</span>
-            </button>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Separated link</a></li>
-            </ul>
-            <input type="text" class="form-control" aria-label="Text input with segmented dropdown button">
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">VR Asset Name</span>
+                <input id="assetName" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+              </div>
+
+
+
+              
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">VR Asset IP</span>
+            <input id="assetIP" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
           </div>
 
-          <div class="input-group">
-            <input type="text" class="form-control" aria-label="Text input with segmented dropdown button">
-            <button type="button" class="btn btn-outline-secondary">Action</button>
-            <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-              <span class="visually-hidden">Toggle Dropdown</span>
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Separated link</a></li>
-            </ul>
-            </div>
           </div>
           <br>
           <div class="container">
-          <button type="button" class="btn btn-success">Deploy</button>
           </div>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Discard</button>
+                <button type="button" class="btn btn-success" onclick="createNewAsset()">Deploy</button>
               </div>
             </div>
           </div>
