83bef05f3dd4ae4ccca6565aa3d900877f6fff43
Mac L <macattackairman@gmail.com>
2022-07-01 12:43:09 -0400

    renamed the index link to interface

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/metafleet/interface?command=dir">Interface</a>
+                    <a class="nav-link active" aria-current="page" href="/interface?command=dir">Interface</a>
                   </li>
                 </ul>
 
