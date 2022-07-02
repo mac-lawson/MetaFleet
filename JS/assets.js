94e9d7cfac59acb1417fa744d66f224b35247eb8
Mac L <macattackairman@gmail.com>
2022-07-02 09:51:02 -0400

    function createNewAsset() {     var assetIP = document.getElementById("assetIP").value;
        var assetName = document.getElementById("assetName").value;
    
        // document.cookie = ((assetIP) + '=' + (assetName));
        // document.cookie = ("headset=" + (assetName));
        //
        //
        //
        //Set Local Storage Items
        localStorage.setItem("Headset IP Address: ", (assetIP));
        localStorage.setItem("Headset Name", (assetName));
        //Set document
        document.getElementById("ACTIVE_DEPLOYS").innerText = ((localStorage.getItem("Headset IP Address"));
    }
    
    function loadAssets() {
        document.getElementById("ACTIVE_DEPLOYS").innerText = (document.cookie);
    }

--- JS/assets.js
+++ JS/assets.js
@@ -4,11 +4,16 @@ function createNewAsset() {
     var assetName = document.getElementById("assetName").value;
 
 
-    document.cookie = ((assetIP) + '=' + (assetName));
-    document.cookie = ("headset=" + (assetName));
-
-
-    document.getElementById("ACTIVE_DEPLOYS").innerText = (document.cookie);
+    // document.cookie = ((assetIP) + '=' + (assetName));
+    // document.cookie = ("headset=" + (assetName));
+    //
+    //
+    //
+    //Set Local Storage Items
+    localStorage.setItem("Headset IP Address: ", (assetIP));
+    localStorage.setItem("Headset Name", (assetName));
+    //Set document
+    document.getElementById("ACTIVE_DEPLOYS").innerText = ((localStorage.getItem("Headset IP Address"));
 }
 
 function loadAssets() {
